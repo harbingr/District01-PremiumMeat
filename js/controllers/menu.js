8fccdedb07df0ac3bb3eeab63f1bd46294458375
Robbert Segers <you@example.com>
2015-04-08 18:21:49 +0200

    section headers responsive, cleaning code (class wrap), menu ingredients array characters cleaned

--- js/controllers/menu.js
+++ js/controllers/menu.js
@@ -5,14 +5,3 @@ angular.module('PremiumMeat').controller('MenuCtrl', function($scope, $http) {
           $scope.menu = res.data;
         });
 });
-
-/*
-var descriptionString = '["Mozarella","Pepperoni","Cheese","Beef","Ketchup"]';
-
-var cleanBracketsLeft = descriptionString.replace(/\[/g, '');
-var cleanBracketsRight = cleanBracketsLeft.replace(/\]/g, '');
-var cleanQuotes = cleanBracketsRight.replace(/\"/g, '');
-var cleanComma = cleanQuotes.replace(/\,/g, ' ');
-
-console.log(cleanComma);
-*/
