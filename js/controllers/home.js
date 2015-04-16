9d122aa8953ebca9742acb1c2a32e08d378a9488
Robbert Segers <you@example.com>
2015-04-16 11:59:22 +0200

    Slider: promo divs now control active slide.

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -5,11 +5,21 @@ var PremiumMeat = angular.module('PremiumMeat')
 
 	.controller('HomeCtrl', function ($scope, $http) {
 
+/*
 		$scope.activeslide = {};
 		$scope.changeActiveSlide =  function changeActiveSlide(i){
 			$scope.activeslide = i;
 			console.log($scope.activeslide);
 		};
+*/
+		$scope.changeActiveSlide = function(i) {
+		    angular.forEach($scope.slides, function(slide) {
+		        slide.active = false; //Desactive all slides
+		        if(slide.id === i+1) {
+		            slide.active = true; //Active the clicked slide
+		        }
+		    });
+		}
 
 		$scope.myInterval = 8000;
 		var slides = $scope.slides = [];
