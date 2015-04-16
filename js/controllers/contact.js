7f1a5ac581b8e056183295f6aeb7f57f0994010f
Robbert Segers <you@example.com>
2015-04-16 17:41:12 +0200

    contact form validation (work in progress)

--- js/controllers/contact.js
+++ js/controllers/contact.js
@@ -1,7 +1,9 @@
 angular.module("PremiumMeat").controller('contactController', function($scope) {
+		console.log("form val");
+		//alert('form val alert');
 	$scope.submitForm = function(isValid) {
 		if (isValid) {
 			alert('our form is amazing');
-		}
+		};
 	};
 });
