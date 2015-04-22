51c47359dd24a8b50e136b3c12921face32d29af
Robbert Segers <you@example.com>
2015-04-22 10:12:20 +0200

    conflict: ng google analytics dependency in comment

--- js/controllers/franchises.js
+++ js/controllers/franchises.js
@@ -4,10 +4,11 @@
 
 var servicepath = "http://franchise.district01.be/api/";
 angular.module('PremiumMeat').controller('FranchisesCtrl', function ($scope, $window, $http) {
-
+	//, $stateParams
 	$scope.franchises = [];
 	$scope.selectedImg = {};
 
+<<<<<<< HEAD
 /*
 	$scope.ImgFallback = function ImgFallback(fallback){
 		if (fallback === "") {
@@ -17,6 +18,9 @@ angular.module('PremiumMeat').controller('FranchisesCtrl', function ($scope, $wi
 */
 
 	//$scope.selectedImg = 'lorempixel.com/output/city-q-c-400-300-1.jpg';
+=======
+	$scope.ID = parseInt($stateParams.locationName);
+>>>>>>> 7f1a5ac581b8e056183295f6aeb7f57f0994010f
 
 	$http.get(servicepath + 'franchises/nl.json').then(function(res){
 		$scope.franchises = res.data;
@@ -25,7 +29,7 @@ angular.module('PremiumMeat').controller('FranchisesCtrl', function ($scope, $wi
 
 		//console.log($scope.franchises[0].name);
 		//console.log($scope.franchises[0].description);
-		console.log($scope.franchises[0].images[0].list);
+		//console.log($scope.franchises[0].images[0].list);
 		//console.log($scope.franchises[0].location[0].long);
 		//console.log($scope.franchises[0].location[0].lat);
 	});
