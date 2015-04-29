11ae00dc74eaa0d40109e823b2adbf1f74efacc4
Robbert Segers <you@example.com>
2015-04-29 10:15:56 +0200

    contact onderwerp dynamisch ingevuld

--- js/controllers/contact.js
+++ js/controllers/contact.js
@@ -1,6 +1,8 @@
 angular.module("PremiumMeat").controller('contactController', function($scope, $stateParams) {
 
 		console.log($stateParams.onderwerp);
+		$scope.contactform = {};
+		$scope.contactform.onderwerp = $stateParams.onderwerp;
 
 	$scope.submitForm = function(isValid) {
 
