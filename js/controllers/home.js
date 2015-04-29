0d8b7a9e7c10a3183681de811e8b24fbbf3db90c
Robbert Segers <you@example.com>
2015-04-29 09:40:57 +0200

    home image slider src fallback directive

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -25,3 +25,15 @@ var PremiumMeat = angular.module('PremiumMeat')
 
 	});
 
+	PremiumMeat.directive('errSrc', function() {
+	  return {
+	    link: function(scope, element, attrs) {
+	      element.bind('error', function() {
+	        if (attrs.src != attrs.errSrc) {
+	          attrs.$set('src', attrs.errSrc);
+	        }
+	      });
+	    }
+	  }
+	});
+
