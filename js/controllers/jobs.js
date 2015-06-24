8abbbffe3ba959abfc72e5bac3418df2f0c45082
Robbert Segers <you@example.com>
2015-06-24 14:13:01 +0200

    Job: selected interactive glyphicon per job (bug: one open at a time)

--- js/controllers/jobs.js
+++ js/controllers/jobs.js
@@ -1,17 +1,20 @@
 
 var servicepath = "http://franchise.district01.be/api/";
 angular.module('PremiumMeat').controller('JobsCtrl', function ($scope, $http) {
+
 	$scope.oneAtATime = true;
+
 
 	$scope.jobs = [];
 	$http.get(servicepath + 'jobs/nl.json').then(function(res){
 		$scope.jobs = res.data;
-  });
+});
 
-  $scope.status = {
-	open: false,
-	isFirstOpen: true
-  };
+$scope.job = { selected: undefined };
 
+	$scope.status = {
+		open: false,
+		isFirstOpen: true
+	};
 
 });
