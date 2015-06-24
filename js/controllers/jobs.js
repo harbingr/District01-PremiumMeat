b93aabfd1aec224815b2d779394afaf16d9d42ff
Robbert Segers <you@example.com>
2015-06-24 15:06:03 +0200

    jobs: expanded job gets active styling (not only hover)

--- js/controllers/jobs.js
+++ js/controllers/jobs.js
@@ -3,18 +3,11 @@ var servicepath = "http://franchise.district01.be/api/";
 angular.module('PremiumMeat').controller('JobsCtrl', function ($scope, $http) {
 
 	$scope.oneAtATime = true;
-
+	$scope.job = { selected: undefined };
+	console.log($scope.oneAtATime);
 
 	$scope.jobs = [];
 	$http.get(servicepath + 'jobs/nl.json').then(function(res){
 		$scope.jobs = res.data;
-});
-
-$scope.job = { selected: undefined };
-
-	$scope.status = {
-		open: false,
-		isFirstOpen: true
-	};
-
+	});
 });
