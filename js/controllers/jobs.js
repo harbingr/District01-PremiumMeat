bf1914fda468749dfa789218322c3d6d0c254750
Robbert Segers <you@example.com>
2015-06-26 11:08:43 +0200

    code cleaning

--- js/controllers/jobs.js
+++ js/controllers/jobs.js
@@ -1,13 +1,16 @@
 
 var servicepath = "http://franchise.district01.be/api/";
 angular.module('PremiumMeat').controller('JobsCtrl', function ($scope, $http) {
+
 	$scope.oneAtATime = true;
 	$scope.jobs = [];
 	$http.get(servicepath + 'jobs/nl.json').then(function(res){
 		$scope.jobs = res.data;
-  });
-  $scope.status = {
-	isFirstOpen: true,
-	isFirstDisabled: false
-  };
+	});
+
+	$scope.status = {
+		isFirstOpen: true,
+		isFirstDisabled: false
+	};
+
 });
