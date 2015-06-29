50677691b34ccda1bfcedfca3c3d9b24e551662d
Robbert Segers <you@example.com>
2015-06-29 18:18:47 +0200

    google map bug fix

--- js/controllers/LocatieDetailCtrl.js
+++ js/controllers/LocatieDetailCtrl.js
@@ -16,4 +16,9 @@ angular.module('PremiumMeat').controller('LocatieDetailCtrl', function ($scope, 
 	$scope.locatieCenter.longitude = $scope.locatieParams.locatieLong;
 	$scope.locatieCenter.zoom = 9;
 
+	/*case: pass by reference. You're returning a single instance of an object that is assigned to center and marker. Angular databinding updates that single instance when center changes, which also changes your marker position.
+
+	you store a reference to the object and return that reference to be used by nggm. nggm watches that object and triggers angular databinding on changes, so it behaves like angular users expects*/
+
+	console.log($scope.locatieCenter);
 });
