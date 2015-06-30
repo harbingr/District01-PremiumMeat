dd8ecd7e3ba72218e61cfe52e37159ba783b8bd9
Robbert Segers <you@example.com>
2015-06-30 13:13:44 +0200

    contact php code delete, $light color adjust

---
+++ save.php
@@ -0,0 +1,14 @@
+<?php
+	print(‘<pre>’);
+	print_r("stuff");
+	print(‘</pre>’);
+
+	var_dump("in php!");
+
+	print_r($_POST);
+
+	$data = file_get_contents("php://input");
+	$myfile = fopen("contact.txt", "w") or die("Unable to open file!");
+	fwrite($myfile, $data);
+	fclose($myfile);
+?>
